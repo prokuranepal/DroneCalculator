4093620fd1d2f9f14c5c3c612a941bd38dbe113d
Swainstha <swainstha@outlook.com>
2021-05-25 10:01:08 +0545

    Resolve for multiple sections

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,54 +1,72 @@
 import React from 'react'
-import { Paper, Typography, TextField, Container } from '@material-ui/core'
+import { Paper, Typography, TextField, Container} from '@material-ui/core'
 import InputAdornment from '@material-ui/core/InputAdornment';
 import '../App.css'
 import Input from '@material-ui/core/Input';
-import { makeStyles } from '@material-ui/core'
+import { makeStyles, withStyles } from '@material-ui/core'
 const useStyles = makeStyles((theme) => ({
   input: {
     display: 'flex',
     justifyContent: 'space-evenly',
     alignItems: 'center',
-    paddingTop: theme.spacing(2),
-    paddingBottom: theme.spacing(2),
+    paddingTop: theme.spacing(0.5),
+    paddingBottom: theme.spacing(0),
     gap: '20px',
 
   },
 
-}))
+}));
+
+const CssTextField = withStyles({
+  root: {
+      '& label.Mui-focused': {
+          color: 'black',
+      },
+      '& .MuiInput-underline:after': {
+          borderBottomColor: 'white',
+      },
+      '& .MuiOutlinedInput-root': {
+          '& fieldset': {
+              borderColor: '#686F76',
+          },
+          '&:hover fieldset': {
+              borderColor: 'green',
+          },
+          '&.Mui-focused fieldset': {
+              borderColor: 'blue',
+              color: 'blue'
+          },
+      },
+  },
+})(TextField);
 const InputUnit = (props) => {
   const classes = useStyles()
   // console.log(props, 'props')
   const { field, unit, name, value, defaultValue } = props.data;
   // console.log(name, 'nameee')
   return (
-    <Container>
+    // <Container>
       <div className="input">
-        <Typography variant="body1"  >{field}</Typography>
-        <Input
+        <Typography style={{textAlign: 'left'}} variant="body1">{field}</Typography>
+        <CssTextField
           id="standard-number"
           label="Enter number only"
           type="number"
           size='small'
-          // defaultValue={defaultValue}
-
+          defaultValue={value}
+          margin="normal"
+          required
           name={name}
           onChange={props.onChange}
-          //   value="100"
-          //   fullWidth
           variant='outlined'
           color='primary'
           value={value}
-          //   helperText="Enter numbers only"
-          InputLabelProps={{
-            shrink: true,
-          }}
-
+          autoFocus
         />
-        <Typography variant="body1">{unit}</Typography>
+        <Typography style={{textAlign: 'left'}} variant="body1">{unit?unit:null}</Typography>
 
       </div>
-    </Container>
+    // </Container>
   )
 }
 export default InputUnit
--- src/components/InputContainer.js
+++ src/components/InputContainer.js
@@ -1,24 +1,28 @@
 import React, { Component } from 'react'
-import { Paper, Typography, TextField, Container } from '@material-ui/core'
+import { Paper, Typography, TextField, Container, Grid } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core'
 import Input from './Input';
 import '../App.css'
+import { specs } from '../data/specs';
 
-// const styles=makeStyles((theme)=>({
-//     input:{
-//         display:'flex',
-//         justifyContent:'space-around',
-//         alignItems:'center',
-//         paddingTop:theme.spacing(2),
-//         paddingBottom:theme.spacing(2)
-//     },
-//     paper:{
-//         margin:theme.spacing(2),
-//         display:'grid',
-//         gridTemplateColumns:'repeat(auto-fit,minmax(600px,1fr))'
-//     }
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    input: {
+        display: 'flex',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        paddingTop: theme.spacing(2),
+        paddingBottom: theme.spacing(2)
+    },
+    paper: {
+        margin: theme.spacing(2),
+        display: 'grid',
+        gridTemplateColumns: 'repeat(auto-fit,minmax(600px,1fr))'
+    }
 
-// }))
+}))
 
 
 
@@ -26,70 +30,7 @@ class InputContainer extends Component {
 
 
     state = {
-        input: {
-            kvrating: {
-                value: '830',
-                field: 'KV Rating',
-                unit: 'kv',
-                defaultValue: 830,
-                name: 'kvrating'
-            },
-            cellsInSeries: {
-                value: '',
-                field: 'Cells in series',
-                unit: 'V',
-                defaultValue: 5,
-                name: 'cellsInSeries'
-            },
-            nominalVoltage: {
-                value: '',
-                field: 'Nominal Voltage',
-                defaultValue: 18.5,
-                name: 'nominalVoltage'
-            },
-            maxRPM: {
-                value: '',
-                field: 'No. load MAX RPM',
-                defaultValue: 15340,
-                name: 'maxRPM'
-            },
-            estimatedMaxPercent: {
-                value: '',
-                field: 'Estimated max percent',
-                unit: '%',
-                defaultValue: 85,
-                name: 'estimatedMaxPercent'
-            },
-            maxWorkingRPM: {
-                value: '',
-                field: 'Max working RPM',
-                unit: '%',
-                defaultValue: null,
-                name: 'maxWorkingRPM'
-            },
-            maxPower: {
-                value: '',
-                field: 'Max power',
-                unit: 'Watt',
-                defaultValue: 1000,
-                name: 'maxPower'
-            },
-            altitude: {
-                value: '',
-                field: 'Altitude',
-                unit: 'm',
-                defaultValue: 0,
-                name: 'altitude'
-            },
-            density: {
-                value: '',
-                field: 'Density',
-                unit: 'kg/m^3',
-                defaultValue: 1.225,
-                name: 'density'
-            }
-
-        }
+        ...specs
     }
 
     changeHandler = (e, data) => {
@@ -102,43 +43,59 @@ class InputContainer extends Component {
         let item = updatedInput[data.id];
 
         //update the value using the value from onChange to the related field
-        item = {...item, value: e.target.value};
+        item = { ...item, value: e.target.value };
 
         //update the field item with updated value to updatedInput
         updatedInput[data.id] = item;
 
         //set the new state
-        this.setState({input: updatedInput});
+        this.setState({ input: updatedInput });
 
     }
     render() {
-        // const classes=styles()
-        // console.log(this.state.input, "updatedinput")
-
         let inputData = []
         for (let key in this.state.input) {
             inputData.push({ id: key, data: this.state.input[key] })
         }
-        // console.log(inputData, "inputdata")
-
-        // console.log(this.state.input, 'input')
+        // const classes = useStyles();
         return (
-            <>
+            <div style={{ margin: '0px 20px' }}>
 
-                <Typography variant='h5' style={{ margin: '12px', textAlign: 'center' }} >{this.props.header}</Typography>
-
-                <Paper className="paper">
-
-                    {inputData.map(eachInputData => {
-                        return (
-                            <Input key={eachInputData.id} data={eachInputData.data} onChange={(e) => this.changeHandler(e, eachInputData)} />
-                        )
-                    })}
+                <div style={{marginTop: '20px'}}>
+                    <Typography variant='h5' style={{ margin: '12px', textAlign: 'center' }} >{this.props.header}</Typography>
+                    <div style={{ flexGrow: 1 }}>
+                        <Paper className="paper" style={{ padding: '20px 30px' }}>
+                            <Grid container spacing={0}>
 
 
-                </Paper>
+                                {inputData.map(eachInputData => {
+                                    return (
+                                        <Grid item xs={6}>
+                                            <Input key={eachInputData.id} data={eachInputData.data} onChange={(e) => this.changeHandler(e, eachInputData)} />
+                                        </Grid>
+                                    )
+                                })}
 
-            </>
+                            </Grid>
+                        </Paper>
+                    </div>
+                </div>
+                <div style={{margin: '20px 0px'}}>
+                    <Typography variant='h5' style={{ margin: '12px', textAlign: 'center' }} >{this.props.header}</Typography>
+
+                    <Paper className="paper" style={{ padding: '0px 20px' }}>
+                        <Grid container>
+                            {inputData.map(eachInputData => {
+                                return (
+                                    <Grid item xs={6} md={6} lg={6}>
+                                        <Input key={eachInputData.id} data={eachInputData.data} onChange={(e) => this.changeHandler(e, eachInputData)} />
+                                    </Grid>
+                                )
+                            })}
+                        </Grid>
+                    </Paper>
+                </div>
+            </div>
         )
     }
 }
